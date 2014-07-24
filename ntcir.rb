b403b8aa7f6401875c1adb6483892e8effa542ab
kitanaisia <kitanaisia@gmail.com>
2014-07-25 00:50:31 +0900

    implement delete_slide

---
+++ ntcir.rb
@@ -0,0 +1,75 @@
+#!ruby
+# -*- coding: utf-8 -*-
+
+module NTCIR
+    # 
+    # delete_slide
+    #   文字数が少ないスライド，一貫性の低いスライドを取り除く
+    #   
+    #   slide_list: 検索対象のファイルリスト
+    #   pmi_hash: 2単語間のPMI値を格納した二重ハッシュ
+    #   
+    #   checked_slide_list: 不要なスライドを取り除いた，検索対象のファイルリスト
+    #
+    def delete_slide(slide_list, pmi_hash)
+        min_char = 100
+        acceptance_rate = 0.5    # 50%
+        
+        checked_slide_list = []
+
+        # 文字数によるスクリーニング
+        slide_list.each { |slide_path| 
+            content = File::open(slide_path).read
+            
+            if content.length <= 100
+                next
+            end
+
+            coherency = calc_coherency(content, pmi_hash)
+            checked_slide_list.push([ slide_path, coherency ])
+        }
+
+
+        # 一貫性によるスクリーニング
+        number_of_accept = (checked_slide_list.length * acceptance_rate).to_int  # 受理するスライドの枚数
+
+        checked_slide_list.sort! { |a,b| b[1]<=>a[1] }
+        checked_slide_list.slice!( 0..(number_of_accept-1) )
+        checked_slide_list.map!{|elem| elem[0]}
+        
+        return checked_slide_list
+    end
+
+    # 
+    # calc_coherency
+    #   文章の一貫性を[1,-1]で計算する．
+    #
+    #   sentence:文章．
+    #   pmi_hash:2単語のPMI値を格納した二重ハッシュ
+    #
+    #   coherency:文章の一貫性
+    #
+    def calc_coherency(sentence, pmi_hash)
+        coherency = 0.0
+        
+        noun_list = getWordList(sentence, "sentence")\
+                    .select{|elem| elem[1] == "名詞"}\
+                    .map{|elem| elem[0]}
+
+        combination_arr = noun_list.combination(2)\
+                               .to_a\
+                               .collect{|elem| elem.sort}\
+                               .uniq
+
+        combination_arr.each { |combination| 
+            word1 = combination[0]
+            word2 = combination[1]
+
+            coherency += pmi_hash[word1][word2]
+        }
+
+        coherency /= combination_arr.length.to_f
+
+        return coherency
+    end
+end
